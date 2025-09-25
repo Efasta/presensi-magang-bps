6595f8dc409b9f549c96afb46f2bd93ec5c54658
Efasta <gantengfaiz004@gmail.com>
2025-09-25 11:37:49 +0800

    Perubahan seeder user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,7 +15,47 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         User::factory()->create([
-            'name' => 'ADMIN',
+            'name' => 'ADMINIPDS',
+            'is_admin' => true,
+            'email' => 'chatbotbpsabsen@gmail.com',
+            'jenis_kelamin' => 'Laki-laki',
+            'password' => Hash::make('admin$%^!@#')
+        ]);
+
+        User::factory()->create([
+            'name' => 'ADMINSDM',
+            'is_admin' => true,
+            'email' => 'chatbotbpsabsen@gmail.com',
+            'jenis_kelamin' => 'Laki-laki',
+            'password' => Hash::make('admin$%^!@#')
+        ]);
+
+        User::factory()->create([
+            'name' => 'ADMINNERACA',
+            'is_admin' => true,
+            'email' => 'chatbotbpsabsen@gmail.com',
+            'jenis_kelamin' => 'Laki-laki',
+            'password' => Hash::make('admin$%^!@#')
+        ]);
+
+        User::factory()->create([
+            'name' => 'ADMINSOSIAL',
+            'is_admin' => true,
+            'email' => 'chatbotbpsabsen@gmail.com',
+            'jenis_kelamin' => 'Laki-laki',
+            'password' => Hash::make('admin$%^!@#')
+        ]);
+
+        User::factory()->create([
+            'name' => 'ADMINPRODUKSI',
+            'is_admin' => true,
+            'email' => 'chatbotbpsabsen@gmail.com',
+            'jenis_kelamin' => 'Laki-laki',
+            'password' => Hash::make('admin$%^!@#')
+        ]);
+
+        User::factory()->create([
+            'name' => 'ADMINDISTRIBUSI',
             'is_admin' => true,
             'email' => 'chatbotbpsabsen@gmail.com',
             'jenis_kelamin' => 'Laki-laki',

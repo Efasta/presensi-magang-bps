e1ea6b327fb080cbc7f62814b5e5585df42ecab0
Efasta <gantengfaiz004@gmail.com>
2025-09-27 10:02:39 +0800

    Perbaikan tombol absen tampilan mobile #10

--- resources/views/absensi/create.blade.php
+++ resources/views/absensi/create.blade.php
@@ -5,7 +5,7 @@
     <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
     <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
 
-    <div class="px-6 mt-3 mb-35 sm:mt-8.5 sm:mb-0">
+    <div class="px-6 mt-[12px] mb-[140px] sm:mt-[34px] sm:mb-[0px]">
         <!-- Wrapper relative agar absolute bekerja -->
         <div class="relative w-full h-[661px]">
             <!-- Map -->

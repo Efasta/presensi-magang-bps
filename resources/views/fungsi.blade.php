92307ab5c0f06cd004e572a34a2196b962d8b96d
Efasta <gantengfaiz004@gmail.com>
2025-10-04 16:37:57 +0800

    perbaikan pesan eror view 403 user pada halaman fungsi.

--- resources/views/fungsi.blade.php
+++ resources/views/fungsi.blade.php
@@ -252,7 +252,7 @@
         <section class="bg-white dark:bg-gray-900">
             <div class="py-8 px-4 mx-auto max-w-screen-xl sm:pt-40 pt-50 sm:px-6">
                 <div class="mx-auto max-w-screen-sm text-center">
-                    <h1 class="mb-4 text-7xl tracking-tight font-extrabold sm:text-9xl text-red-600">
+                    <h1 class="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-red-600">
                         403</h1>
                     <p class="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">Akses
                         Ditolak</p>

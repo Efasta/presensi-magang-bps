02ecad8314be035c70b0bea299f2ce9c07129650
Efasta <gantengfaiz004@gmail.com>
2025-10-04 16:41:32 +0800

    Perbaikan posisi pesan eror 403 view user pada halaman fungsi.

--- resources/views/fungsi.blade.php
+++ resources/views/fungsi.blade.php
@@ -250,7 +250,7 @@
     @else
         {{-- Optional: Jika kamu mau tampilkan pesan untuk non-admin --}}
         <section class="bg-white dark:bg-gray-900">
-            <div class="py-8 px-4 mx-auto max-w-screen-xl sm:pt-40 pt-50 sm:px-6">
+            <div class="py-8 px-4 mx-auto max-w-screen-xl lg:pt-40 pt-50 lg:px-6">
                 <div class="mx-auto max-w-screen-sm text-center">
                     <h1 class="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-red-600">
                         403</h1>
